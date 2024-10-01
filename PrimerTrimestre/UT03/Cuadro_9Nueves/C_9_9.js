38064889c99532fdbb4d28000fd1e71cf277bab9
IONECOV <ionealf.cov@educa.jcyl.es>
2024-10-01 14:42:20 +0200

    C5 no entiendo nada

---
+++ PrimerTrimestre/UT03/Cuadro_9Nueves/C_9_9.js
@@ -0,0 +1,17 @@
+function devolverNumeros(minimo, maximo) {
+
+}
+
+function cargarNumeros() {
+    let completadoNumero = [null, false, false, false, false,
+        false, false, false, false, false];
+    for (let i = 0; i < 50; i++) {
+        devolverNumeros(1, 9);
+    }
+}
+
+
+/*
+window.onload = function(){
+    alert("hola")
+}*/
