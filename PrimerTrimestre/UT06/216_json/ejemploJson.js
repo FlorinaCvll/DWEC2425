ff89da339c3dad466be81c97794a3e03a8e39aef
IONECOV <ionealf.cov@educa.jcyl.es>
2024-11-04 12:54:20 +0100

    C16 que e esto

---
+++ PrimerTrimestre/UT06/216_json/ejemploJson.js
@@ -0,0 +1,15 @@
+function jsonAJavaScript() {
+    const estructuraJson =
+        '{'
+        + ' "titulo":"Manual de UFOlogía
+}
+
+
+window.onload = function () {
+    document
+        .getElementById("jsonAJS")
+        .addEventListener("click", jsonAJavaScript);
+    document
+        .getElementById("JSaJson")
+        .addEventListener("click", JavaScriptAJson);
+};
