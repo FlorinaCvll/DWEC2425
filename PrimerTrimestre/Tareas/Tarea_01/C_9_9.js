cb239f2fc6c29148d5a14a64cdf890fd198e00a6
IONECOV <ionealf.cov@educa.jcyl.es>
2024-11-11 10:35:07 +0100

    C20 nieeeeet

--- PrimerTrimestre/Tareas/Tarea_01/C_9_9.js
+++ PrimerTrimestre/Tareas/Tarea_01/C_9_9.js
@@ -1,17 +1,31 @@
-function devolverNumeros(minimo, maximo) {
+function rellenarCuadrado() {
+    const ids = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3'];
+    const valores = generarValores(1, 9);
+
+    ids.forEach((id, index) => {
+        const div = document.getElementById(id);
+        div.textContent = valores[index];
+    });
 
 }
 
-function cargarNumeros() {
-    let completadoNumero = [null, false, false, false, false,
-        false, false, false, false, false];
-    for (let i = 0; i < 50; i++) {
-        devolverNumeros(1, 9);
+function generarValores(minimo, maximo) {
+    const numeros = [];
+    const tam = 9;
+    while (numeros.length < tam) {
+        let num = generarNumRandom(minimo, maximo);
+        if (!numeros.includes(num)) {
+            numeros.push(num);
+        }
     }
+    return numeros;
 }
 
+function generarNumRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 
-/*
-window.onload = function(){
-    alert("hola")
-}*/
+}
+
+window.onload = function () {
+    document.getElementById("enviar").addEventListener("click", rellenarCuadrado);
+}
