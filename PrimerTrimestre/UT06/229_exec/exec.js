36ae8e84f7250498147f96af86ea24826aa09518
IONECOV <ionealf.cov@educa.jcyl.es>
2024-11-13 12:03:47 +0100

    C22 andiamo

--- PrimerTrimestre/UT06/229_exec/exec.js
+++ PrimerTrimestre/UT06/229_exec/exec.js
@@ -1,11 +1,12 @@
 function ejecuarExec() {
-    let texto = "Este es un texto de prueba";
-    //let expresion = /\w+/;
-    // let expresion = /Este/;
-    let expresion = /(\w+)/;
+    let texto = "Este es un texto  Este de prueba";
+    //let expresion = /\w+/;  // w+ busca una palabra
+    // let expresion = /Este/; // un → busca la palabra un
+    //let expresion = /(\w+)(\s)/; // busca palabra seguida de espacio
+    let expresion = /(\s)(Este)/;
     let res = expresion.exec(texto);
-    console.log(res);
-    console.log(res[0]);
+    console.log(res); // Muestra el array completo
+    console.log(res[0]); // 1ª coincidencia, que es la completa
 }
 
 window.onload = function () {
