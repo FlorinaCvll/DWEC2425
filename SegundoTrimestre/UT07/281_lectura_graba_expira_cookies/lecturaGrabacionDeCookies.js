993d1c34738fd253cc802ebb4697b694dae2c7d1
IONECOV <ionealf.cov@educa.jcyl.es>
2024-12-10 14:40:13 +0100

    c35

--- SegundoTrimestre/UT07/281_lectura_graba_expira_cookies/lecturaGrabacionDeCookies.js
+++ SegundoTrimestre/UT07/281_lectura_graba_expira_cookies/lecturaGrabacionDeCookies.js
@@ -1,20 +1,36 @@
 function crearCookies() {
     document.cookie = "user=John; SameSite=Strict";
     console.log(document.cookie);
-    document.cookie = "user=Jane; SameSite=Lax";
+    document.cookie = "user=Jane; SameSite=Strict";
+    console.log(document.cookie);
+    document.cookie = "user=Jack; SameSite=Strict";
     console.log(document.cookie);
 }
 
 function recuperarCookies() {
-
+    let arrCookies = document.cookie.split("; ");
+    for (const cookie of arrCookies) {
+        let [nombre, valor] = cookie.split("=");
+        console.log(`La cookie "${nombre}" tiene el valor "${valor}"`);
+    }
 }
 
 function expirarCookie() {
+    let hoy = new Date();
+    let caducidadMs = hoy.getTime() + 1000 * 60 * 60 * 24 * 7; // por 1000 para convertirlo en milisegundos
+    let caducidad = new Date(caducidadMs);
+    console.log("→" + caducidad);
+    document.cookie = `user=John; SameSite=Strict; expires=${caducidad.toUTCString()}`;
+}
 
+function borrarCookie() {
+    document.cookie = "user=John; SameSite=Strict; expires=Sat, 01 Jan 2000 00:00:01 GMT";
+    console.log(document.cookie);
 }
 
 window.onload = function () {
     document.getElementById("crearCookie").addEventListener("click", crearCookies);
     document.getElementById("recuperarCookie").addEventListener("click", recuperarCookies);
     document.getElementById("expirarCookie").addEventListener("click", expirarCookie);
+    document.getElementById("borrarCookie").addEventListener("click", borrarCookie);
 }
