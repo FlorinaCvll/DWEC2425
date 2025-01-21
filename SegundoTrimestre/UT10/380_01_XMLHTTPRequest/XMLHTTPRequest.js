702f1986c16a974a0aec4f71b7013c2d693e560b
IONECOV <ionealf.cov@educa.jcyl.es>
2025-01-21 14:37:02 +0100

    c210125

--- SegundoTrimestre/UT10/380_01_XMLHTTPRequest/XMLHTTPRequest.js
+++ SegundoTrimestre/UT10/380_01_XMLHTTPRequest/XMLHTTPRequest.js
@@ -2,12 +2,13 @@ function cargarXMLDoc() {
     let xhttp = new XMLHttpRequest();
     document.getElementById("demo").innerHTML = "Texto cargado de forma asincrona<br>";
     xhttp.onreadystatechange = function () {
-        alert("-Hola mundo --> " + xhttp.readyState);
+        //alert("-Hola mundo --> " + xhttp.readyState);
         if (xhttp.readyState === 4) {
             if (xhttp.status === 200) {
                 document.getElementById("demo").innerHTML += "<br>" + xhttp.responseText.replace(/\n/g, "<br>");
             }
         }
+        alert("+Hola mundo --> " + xhttp.readyState);
     };
     xhttp.open("GET", "Hello.txt", true);
     xhttp.send();
---
+++ SegundoTrimestre/UT10/385_ajax_texto/ajaxTexto.js
@@ -0,0 +1,9 @@
+function ajaxTexto() {
+    alert("Ajax Texto");
+    let pre = document.querySelector("pre");
+
+}
+
+window.onload = function () {
+    document.getElementById("ajaxTexto").addEventListener("click", ajaxTexto);
+}
